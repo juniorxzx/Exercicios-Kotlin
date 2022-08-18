40803936eea5c76006f6bb277f8bd7dfd119690b
Junior <alexanroju@hotmail.com>
2022-08-18 10:50:00 -0300

    18-08-2022

--- 18-08-2022/src/main/kotlin/Calculadora.kt
+++ 18-08-2022/src/main/kotlin/Calculadora.kt
@@ -1,48 +1,60 @@
 fun calcular(escolha: Int): Any {
-        return if(escolha == 1) {
-            var total = 0
-            print("Digite o primeiro número: ")
-            val n1 = readln().toInt()
-            print("Digite o segundo número: ")
-            val n2 = readln().toInt()
+        return when(escolha){
 
-            total = n1 + n2
 
-            println("O resultado da soma é $total")
-        }else if(escolha == 2){
-            var total = 0
-            print("Digite o primeiro número: ")
-            val n1 = readln().toInt()
-            print("Digite o segundo número: ")
-            val n2 = readln().toInt()
+            1 -> {
+                var total = 0
+                print("Digite o primeiro número: ")
+                val n1 = readln().toInt()
+                print("Digite o segundo número: ")
+                val n2 = readln().toInt()
 
-            total = n1 - n2
+                total = n1 + n2
 
-            println("O resultado da soma é $total}")
-        } else if(escolha == 3){
-            var total = 0
-            print("Digite o primeiro número: ")
-            val n1 = readln().toInt()
-            print("Digite o segundo número: ")
-            val n2 = readln().toInt()
+                println("O resultado da soma é $total")
+            }
+            2 -> {
+                var total = 0
+                print("Digite o primeiro número: ")
+                val n1 = readln().toInt()
+                print("Digite o segundo número: ")
+                val n2 = readln().toInt()
 
-            total = n1 / n2
+                total = n1 - n2
 
-            println("O resultado da soma é $total")
-        }else if(escolha == 4){
-            var total = 0
-            print("Digite o primeiro número: ")
-            val n1 = readln().toInt()
-            print("Digite o segundo número: ")
-            val n2 = readln().toInt()
+                println("O resultado da soma é $total")
+            }
+            3 -> {
+                var total = 0
+                print("Digite o primeiro número: ")
+                val n1 = readln().toInt()
+                print("Digite o segundo número: ")
+                val n2 = readln().toInt()
 
-            total = n1 * n2
+                total = n1 / n2
 
-            println("O resultado da soma é $total")
-        }else{
-            return 0
+                println("O resultado da soma é $total")
+            }
+            4 -> {
+                var total = 0
+                print("Digite o primeiro número: ")
+                val n1 = readln().toInt()
+                print("Digite o segundo número: ")
+                val n2 = readln().toInt()
+
+                total = n1 * n2
+
+                println("O resultado da soma é $total")
+            }
+
+            5 -> {
+                println("Fechando calculadora...")
+
+            }
+            else -> {
+
+            }
         }
-
 }
 
 fun main(){
@@ -51,10 +63,10 @@ fun main(){
             "1 - Somar\n" +
             "2 - Subtrair\n" +
             "3 - Dividir\n" +
-            "4 - Multiplicar\n")
+            "4 - Multiplicar\n" +
+            "5 - Fechar calculadora.")
     val escolha = readln().toInt()
 
     calcular(escolha)
-
 
 }
